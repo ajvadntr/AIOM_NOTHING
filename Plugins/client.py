fa0c5a83180ae2f79b1f569ae27c7bbfd718be99
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-01-02 23:45:33 +0530

    Update and rename cofing.py to client.py

--- Plugins/client.py
+++ Plugins/client.py
@@ -1,6 +1,8 @@
-import os
+from pyrogram import Client, filters
 
-class Config(object):
+bot = Client(
+  "My First Project"
   APP_ID = 6146411
   API_HASH = "279c5805accac5a35f6bc8c2e38ac036"
   TG_BOT_TOKEN = "5005535376:AAFvB5dkwxU1Vs3S02oCuogWTN_TSKWQcIw"
+)
