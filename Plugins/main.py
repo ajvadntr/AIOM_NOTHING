83774ebafd5130f788855fa0cfbd86fab7399840
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-01-02 23:53:44 +0530

    Update main.py

--- Plugins/main.py
+++ Plugins/main.py
@@ -2,3 +2,4 @@ from plugins.client import Client
 
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot, message):
+    bot.send_message(message.chat.id, "Hi, This Is A Test Bot")
