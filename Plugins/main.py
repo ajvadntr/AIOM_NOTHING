000c9d093c6cde564a05fed6201561ba8736d693
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-01-03 00:30:20 +0530

    Update main.py

--- Plugins/main.py
+++ Plugins/main.py
@@ -5,7 +5,7 @@ def command1(bot, message):
     bot.send_message(message.chat.id, "Hi, This Is A Test Bot")
 
 @bot.on_message(filters.command('help'))
-def command1(bot, message):
+def command2(bot, message):
     message.replay_text("Their Is Nothing To Say")
 
 @bot.on_message(filters.text)
@@ -19,5 +19,9 @@ WELCOME_MESSAGE = "Hey, Welcome To Our Group"
 @bot.on_message(filters.chat(GROUP) & filters.new_chat_members)
 def welcomebot(client, message):
     message.replay_text(WELCOME_MESSAGE)
+
+@bot.on_message(filters.command('photo'))
+def command3(bot, message):
+    bot.send_photo(message.chat.id, "https://telegra.ph/file/0665b36e85d979ae3e862.jpg")
 
 bot.run()
