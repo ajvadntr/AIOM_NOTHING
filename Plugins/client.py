bc68a0fb48a272c1a9834f5fc282b7cdfd30e2e2
ajvadntr <91082660+ajvadntr@users.noreply.github.com>
2022-01-03 01:54:48 +0530

    Update client.py

--- Plugins/client.py
+++ Plugins/client.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 bot = Client(
   "My First Project"
-  APP_ID = 6146411
+  APP_ID = '6146411'
   API_HASH = "279c5805accac5a35f6bc8c2e38ac036"
   TG_BOT_TOKEN = "5005535376:AAFvB5dkwxU1Vs3S02oCuogWTN_TSKWQcIw"
 )
