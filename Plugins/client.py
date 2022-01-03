bdadc492108212771844e39ebd081ea56b531845
ajvadntr <91082660+ajvadntr@users.noreply.github.com>
2022-01-03 13:11:55 +0530

    Update client.py

--- Plugins/client.py
+++ Plugins/client.py
@@ -1,4 +1,8 @@
 import pyrogram
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 from pyrogram import Client, filters
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from Plugins.Config import Config
