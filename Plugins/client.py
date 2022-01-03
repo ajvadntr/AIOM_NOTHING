762772e9d419b9e210b80e649caaa1a62a72d4fc
ajvadntr <91082660+ajvadntr@users.noreply.github.com>
2022-01-03 12:35:56 +0530

    Update client.py

--- Plugins/client.py
+++ Plugins/client.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 bot = Client(
   "My First Project"
-  API_ID = '6146411'
-  API_HASH = "279c5805accac5a35f6bc8c2e38ac036"
-  TG_BOT_TOKEN = "5005535376:AAFvB5dkwxU1Vs3S02oCuogWTN_TSKWQcIw"
+  api_id= '6146411'
+  api_hash= "279c5805accac5a35f6bc8c2e38ac036"
+  bot_token= "5005535376:AAFvB5dkwxU1Vs3S02oCuogWTN_TSKWQcIw"
 )
